0a1d3a78cd6e9243f3595a2fc227d80adbb11f1d
v-tank <v.tank@yahoo.com>
2019-06-28 18:37:25 -0700

    cleanup and refactor code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(e) {
+  // initialize arrays
   let toBuy = [];
   let bought = [];
 
+  // on-click listener to add item
   $(document).on('click', '#btn', function(e) {
     e.preventDefault();
     let val = $("input[name=grocery-item]").val().trim();
@@ -13,39 +15,38 @@ $(document).ready(function(e) {
     }
   });
 
+  // on-click listener to remove item
   $(document).on('click', '.remove', function(e) {
     e.preventDefault();
 
     let $idx = $(this).attr("data-index");
-    let val = toBuy.splice($idx, 1);
-    bought.push(val);
-    console.log(toBuy);
-    console.log('*'.repeat(20));
-    console.log(bought);
-
-    displayItems();
+    let val = toBuy.splice($idx, 1);  // remove item from `toBuy` array
+    bought.push(val); // add the removed value to `bought` array
+    displayItems();   // refresh display
   });
 
+  // function to add item to list
   function addItemToList(val) {
     toBuy.push(val);
-    console.log(toBuy);
   }
 
+  // clear textbox
   function clearInput() {
     $("input[name=grocery-item").val('');
   }
 
+  // function to display items to screen
   function displayItems() {
-    let $target = $("#list");
-    $target.empty();
+    let $target = $("#list"); // create reference to list on HTML page
+    $target.empty();  // empty out the list before appending
 
-    // Show items left to do
+    // if either of the arrays have values, display them
     if ((toBuy.length > 0) || (bought.length > 0)) {
 
+      // loop through array and append the item to screen
       toBuy.forEach((todo, index) => {
         let $el = $("<li>");
-        $el.html(todo + " <button class='remove'  data-index='" + index + "'>x</button>");
-
+        $el.html("<button class='remove' data-index='" + index + "'>x</button>" + todo);
         $target.append($el);
       });
 
@@ -56,7 +57,8 @@ $(document).ready(function(e) {
       });
 
     } else {
-      $target.append('No items in the list. Add something!');
+      // if no items are found, display below message
+      $target.append('No items in the list. Add something before you forget!');
     }
   };
 
