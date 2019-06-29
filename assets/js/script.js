5af5d789d582e4e4b6da2576319a1c7e639837d8
v-tank <v.tank@yahoo.com>
2019-06-28 19:05:12 -0700

    add localstorage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(e) {
   // initialize arrays
-  let toBuy = [];
-  let bought = [];
+  let toBuy, bought;
 
   // on-click listener to add item
   $(document).on('click', '#btn', function(e) {
@@ -21,13 +20,15 @@ $(document).ready(function(e) {
 
     let $idx = $(this).attr("data-index");
     let val = toBuy.splice($idx, 1);  // remove item from `toBuy` array
-    bought.push(val); // add the removed value to `bought` array
+    bought.push(val[0]); // add the removed value to `bought` array
+    syncLocalStorage(); // sync with localStorage
     displayItems();   // refresh display
   });
 
   // function to add item to list
   function addItemToList(val) {
     toBuy.push(val);
+    syncLocalStorage(); // sync with localStorage
   }
 
   // clear textbox
@@ -62,5 +63,26 @@ $(document).ready(function(e) {
     }
   };
 
+  // refresh localStorage to match user actions
+  function syncLocalStorage() {
+    localStorage.setItem("toBuy", JSON.stringify(toBuy));
+    localStorage.setItem("bought", JSON.stringify(bought));
+  }
+
+  // fetch items from localStorage
+  function fetchLocalStorage() {
+    // if no items found (upon initial visit), set arrays to empty
+    if (localStorage.getItem("toBuy") === null && localStorage.getItem("bought") === null) {
+      toBuy = [];
+      bought = [];
+    } else {
+      // else, parse the string and set equal to the arrays
+      toBuy = JSON.parse(localStorage.getItem("toBuy"));
+      bought = JSON.parse(localStorage.getItem("bought"));
+    }
+  }
+
+  // check if items exist in localStorage upon page-load
+  fetchLocalStorage();
   displayItems();
 });
